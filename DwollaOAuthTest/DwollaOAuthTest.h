8336adf1b3389b253f66a10c05fe05e24a5864a8
Nick Schulze <nicks@dwolla.com>
2012-06-22 14:13:13 -0500

    Added Unit Testing
    Unit Tests

---
+++ DwollaOAuthTest/DwollaOAuthTest.h
@@ -0,0 +1,13 @@
+//
+//  DwollaOAuthTest.h
+//  DwollaOAuthTest
+//
+//  Created by Nick Schulze on 6/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DwollaOAuthTest : SenTestCase
+
+@end
